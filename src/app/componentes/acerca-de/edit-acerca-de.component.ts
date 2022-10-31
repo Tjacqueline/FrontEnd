19ef683516cf257ff14afee93c52757a1a04aae5
Tjacqueline <vtamarajacqueline@gmail.com>
2022-10-31 20:56:55 -0300

    img

--- src/app/componentes/acerca-de/edit-acerca-de.component.ts
+++ src/app/componentes/acerca-de/edit-acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { persona } from 'src/app/model/persona.model';
+import { ImageService } from 'src/app/service/image.service';
 import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
@@ -12,7 +13,8 @@ export class EditAcercaDeComponent implements OnInit {
 persona: persona = null;
   constructor(private activatedRouter: ActivatedRoute,
     private personaService: PersonaService,
-    private router: Router) { }
+    private router: Router,
+    public imageService: ImageService) { }
 
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
@@ -39,6 +41,8 @@ persona: persona = null;
   }
 
   uploadImage($event:any){
-
+   const id = this.activatedRouter.snapshot.params['id'];
+   const name= "perfil_" + id;
+   this.imageService.uploadImage($event, name)
   }
 }
